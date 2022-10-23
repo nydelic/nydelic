1a98c65e79656b1fe362f84b57dd2dd3d90ece24
josias-r <josias.ribi@gmail.com>
2022-10-23 18:46:35 +0200

    fix: use css vars for target

--- src/components/assets/Target.tsx
+++ src/components/assets/Target.tsx
@@ -11,49 +11,49 @@ function Target({}: TargetProps) {
       >
         <path
           d="M 78.727 7.997 C 118.87 7.997 151.413 40.795 151.413 81.253 C 151.413 121.712 118.87 154.51 78.727 154.51 C 38.584 154.51 6.041 121.712 6.041 81.253 C 6.041 40.795 38.584 7.997 78.727 7.997 Z"
-          fill="hsl(206, 100%, 50%)"
+          className="fill-accent-color"
         ></path>
         <path
           d="M 78.774 22.834 C 110.483 22.834 136.188 48.741 136.188 80.699 C 136.188 112.657 110.483 138.565 78.774 138.565 C 47.065 138.565 21.359 112.657 21.359 80.699 C 21.359 48.741 47.065 22.834 78.774 22.834 Z"
-          fill="#f7f5f2"
+          className="fill-white"
         ></path>
         <path
           d="M 79.896 39.211 C 102.633 39.211 121.065 57.786 121.065 80.699 C 121.065 103.613 102.633 122.188 79.896 122.188 C 57.158 122.188 38.726 103.613 38.726 80.699 C 38.726 57.786 57.158 39.211 79.896 39.211 Z"
-          fill="hsl(206, 100%, 50%)"
+          className="fill-accent-color"
         ></path>
         <path
           d="M 79.09 56.379 C 93.464 56.379 105.117 68.121 105.117 82.607 C 105.117 97.092 93.464 108.835 79.09 108.835 C 64.716 108.835 53.063 97.092 53.063 82.607 C 53.063 68.121 64.716 56.379 79.09 56.379 Z"
-          fill="#f7f5f2"
+          className="fill-white"
         ></path>
         <path
           d="M 78.845 73.069 C 84.072 73.069 88.309 77.339 88.309 82.607 C 88.309 87.874 84.072 92.144 78.845 92.144 C 73.618 92.144 69.381 87.874 69.381 82.607 C 69.381 77.339 73.618 73.069 78.845 73.069 Z"
-          fill="hsl(206, 100%, 50%)"
+          className="fill-accent-color"
         ></path>
       </svg>
 
       <svg xmlns="http://www.w3.org/2000/svg" width="203" height="205">
         <path
           d="M 101.5 10.898 L 128.732 66.248 L 189.623 75.123 L 145.562 118.207 L 155.963 179.042 L 101.5 150.319 L 47.037 179.042 L 57.438 118.207 L 13.377 75.123 L 74.268 66.248 Z"
-          fill="hsl(206, 100%, 50%)"
+          className="fill-accent-color"
         ></path>
         <path
           d="M 92.658 0 L 119.89 55.349 L 180.781 64.225 L 136.72 107.308 L 147.121 168.143 L 92.658 139.421 L 38.195 168.143 L 48.597 107.308 L 4.535 64.225 L 65.427 55.349 Z"
           transform="translate(8.842 10.898) rotate(180 92.658 92.947)"
-          fill="hsl(206, 100%, 50%)"
+          className="fill-accent-color"
         ></path>
         <path
           d="M 92.342 0 L 119.481 55.539 L 180.165 64.445 L 136.254 107.675 L 146.62 168.718 L 92.342 139.898 L 38.065 168.718 L 48.431 107.675 L 4.52 64.445 L 65.204 55.539 Z"
           transform="translate(9.158 10.581) rotate(90 92.342 93.265)"
-          fill="hsl(206, 100%, 50%)"
+          className="fill-accent-color"
         ></path>
         <path
           d="M 92.342 0 L 119.481 55.539 L 180.165 64.445 L 136.254 107.675 L 146.62 168.718 L 92.342 139.898 L 38.065 168.718 L 48.431 107.675 L 4.52 64.445 L 65.204 55.539 Z"
           transform="translate(9.158 10.581) rotate(90 92.342 93.265)"
-          fill="hsl(206, 100%, 50%)"
+          className="fill-accent-color"
         ></path>
         <path
           d="M 8.235 103.846 L 63.773 76.707 L 72.679 16.023 L 115.91 59.934 L 176.953 49.568 L 148.133 103.846 L 176.953 158.123 L 115.91 147.757 L 72.679 191.669 L 63.773 130.984 Z"
-          fill="hsl(206, 100%, 50%)"
+          className="fill-accent-color"
         ></path>
       </svg>
       <svg
@@ -64,37 +64,37 @@ function Target({}: TargetProps) {
       >
         <path
           d="M 97.466 29.193 L 100.198 32.679 L 19.177 95.77 L 16.445 92.284 Z"
-          fill="var(--token-de0699f8-96cc-42fa-a87a-eae0cbf16c2d, rgb(33, 38, 39))"
+          className="fill-shade-900"
         ></path>
         <path
           d="M 2.896 0 L 5.891 3.6 L 2.872 20.328 L 0 16.185 Z"
           transform="translate(103.174 26.07) rotate(88 2.945 10.164)"
-          fill="var(--token-de0699f8-96cc-42fa-a87a-eae0cbf16c2d, rgb(33, 38, 39))"
+          className="fill-shade-900"
         ></path>
         <path
           d="M 13.879 0 L 16.41 3.766 L 2.868 3.894 L 0 0.192 Z"
           transform="translate(86.327 21.29) rotate(88 8.205 1.947)"
-          fill="var(--token-de0699f8-96cc-42fa-a87a-eae0cbf16c2d, rgb(33, 38, 39))"
+          className="fill-shade-900"
         ></path>
         <path
           d="M 13.879 0 L 16.41 3.766 L 2.868 3.894 L 0 0.192 Z"
           transform="translate(81.327 25.29) rotate(88 8.205 1.947)"
-          fill="var(--token-de0699f8-96cc-42fa-a87a-eae0cbf16c2d, rgb(33, 38, 39))"
+          className="fill-shade-900"
         ></path>
         <path
           d="M 13.879 0 L 16.41 3.766 L 2.868 3.894 L 0 0.192 Z"
           transform="translate(76.327 29.29) rotate(88 8.205 1.947)"
-          fill="var(--token-de0699f8-96cc-42fa-a87a-eae0cbf16c2d, rgb(33, 38, 39))"
+          className="fill-shade-900"
         ></path>
         <path
           d="M 2.896 0 L 5.891 3.6 L 2.872 20.328 L 0 16.185 Z"
           transform="translate(98.174 30.07) rotate(88 2.945 10.164)"
-          fill="var(--token-de0699f8-96cc-42fa-a87a-eae0cbf16c2d, rgb(33, 38, 39))"
+          className="fill-shade-900"
         ></path>
         <path
           d="M 2.896 0 L 5.891 3.6 L 2.872 20.328 L 0 16.185 Z"
           transform="translate(93.174 34.07) rotate(88 2.945 10.164)"
-          fill="var(--token-de0699f8-96cc-42fa-a87a-eae0cbf16c2d, rgb(33, 38, 39))"
+          className="fill-shade-900"
         ></path>
       </svg>
     </div>
